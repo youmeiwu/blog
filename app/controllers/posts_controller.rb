1d58890b61690ac8058c7185054544f946a16140
sarah <smile-wu@hotmail.com>
2017-10-17 17:01:00 +0800

    complete posts CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Update successfully."
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Delete successfully."
+    redirect_to root_path
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
